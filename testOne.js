1006870c77ebf86b012b5ca04d4c7ac9dbfa7696
Bill Brinkert <william.brinkert@akqa.com>
2018-04-02 12:20:34 -0700

    wip: add some examples

--- testOne.js
+++ testOne.js
@@ -1,9 +1,27 @@
 'use strict';
 
-const getDataSetOne = require('./lib/OneExample');
+const exampleFunctions = require('./lib/OneExample');
+const getDataSetOne = exampleFunctions.getDataSetOne;
+const getDataSetTwo = exampleFunctions.getDataSetTwo;
 
+let names = ['Max', 'Alex', 'Sam'];
 
-getDataSetOne('Max')
-  .then(res => {
-    console.log(`set 1: ${res}`);
-  });
+const processPerson = name => {
+  getDataSetOne(name)
+    .then(responseOne => {
+      console.log(`${name} set 1: ${responseOne}`);
+      var a = new Set(responseOne);
+      getDataSetTwo(name)
+        .then(responseTwo => {
+          console.log(`${name} set 2: ${responseTwo}`);
+          var b = new Set(responseTwo);
+          let unionSet = new Set([...a, ...b]);
+          let unionArray = [...unionSet];
+          console.log(`${name} union: ${unionArray}`);
+        });
+    });
+}
+
+names.forEach(name => {
+  processPerson(name);
+});
